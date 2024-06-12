2ee04fc1904354b22631ca375c5a57c397d515d0
A1in <xusenlin@bilibili.com>
2024-06-12 15:11:45 +0800

    init

---
+++ app/smsff/internal/service/smsff.go
@@ -0,0 +1,39 @@
+package service
+
+import (
+	"a1in-bot/app/smsff/api"
+	"context"
+	qqbot "qqbot/api"
+
+	"github.com/go-kratos/kratos/v2/log"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+// SmsFFService is a greeter service.
+type SmsFFService struct {
+	api.UnimplementedSmsFFServer
+
+	proxy qqbot.ProxyClient
+}
+
+func NewSmsFFService() *SmsFFService {
+	conn, err := grpc.Dial("127.0.0.1:9000", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+	return &SmsFFService{
+		proxy: qqbot.NewProxyClient(conn),
+	}
+}
+
+func (s *SmsFFService) SendSms(ctx context.Context, req *api.SendSmsReq) (resp *api.SendSmsResp, err error) {
+	resp = &api.SendSmsResp{}
+	defer log.Context(ctx).Infof("[SmsFFService] req: %v, resp: %v, err: %v")
+	_, err = s.proxy.SendGroupMsg(ctx, &qqbot.SendGroupMsgReq{
+		GroupId:    253016320,
+		Message:    req.GetText(),
+		AutoEscape: false,
+	})
+	return
+}
